6e46b5eb7c67aa7de208d111894932a178faaeaf
Martyna Michno <mjjmichno@gmail.com>
2019-12-18 14:13:36 +0100

    App skeleton prepared, two packages added to package.json

--- aplikacja/src/App.js
+++ aplikacja/src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Appbar />
+        <Switch>
+          <Route strict sensitive path="/" component={} />
+          <Redirect exact from="/home" to="/" />
+          <Route component={() => <h1>404</h1>} />
+        </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
