efc0df94a00aa166cf63f78239480efa935f952e
Martyna Michno <mjjmichno@gmail.com>
2019-12-19 22:17:23 +0100

    photos cards added using semantic-ui-react

--- aplikacja/src/components/PhotosPanel.js
+++ aplikacja/src/components/PhotosPanel.js
@@ -1,7 +1,24 @@
 import React from 'react'
 import { fetchPhotos } from '../services/PhotosService';
 import { ShowLoader } from "./Loader.js"
+import { Card, Image} from 'semantic-ui-react'
 
+const Photo = props => {
+    const {
+      photoId,
+      picture,
+      name
+    } = props;
+  
+    return (
+      <Card>
+        <Image src={picture} wrapped ui={false} />
+        <Card.Content>
+          <Card.Header>{name}</Card.Header>
+        </Card.Content>
+      </Card>
+    );
+  };
 class PhotosPanel extends React.Component {
 
     constructor(props) {
@@ -21,13 +38,23 @@ class PhotosPanel extends React.Component {
         return this.state.photos ?
             <article className={"article"}>
                 <h1>Hello, check photos!</h1>
-                {this.state.photos.map(photo => {
+                <Card.Group>
+                {this.state.photos.map(photo => (
+                  <Photo
+                    key={photo.photoId}
+                    photoId={photo.photoId}
+                    picture={photo.tinyUrl}
+                    name={photo.name}
+                  />
+                ))}
+              </Card.Group>
+                {/* {this.state.photos.map(photo => {
                     return (
                         <li>
                             {photo.name}
                         </li>
                     );
-                })}
+                })} */}
             </article>
 
             :
