f4f05f2c1425c482e81a9d8aa348f051940df700
Martyna Michno <mjjmichno@gmail.com>
2019-12-20 13:10:06 +0100

    Basic homepage added

--- aplikacja/src/App.js
+++ aplikacja/src/App.js
@@ -9,6 +9,7 @@ import Appbar from "./components/Appbar";
 import Navbar from "./components/Navbar";
 import UserPanel from "./components/UserPanel"
 import PhotosPanel from "./components/PhotosPanel";
+import Home from "./components/Home";
 
 const App = () => {
   return (
@@ -20,6 +21,7 @@ const App = () => {
           <Redirect exact from="/home" to="/" />
           <Route strict exact path="/users/:id/albums" component={UserPanel}/>
           <Route strict exact path="/users/:id/albums/:albumId" component={PhotosPanel}/>
+          <Route strict exact path="/" component={Home}/>
           <Route component={() => <h1>404</h1>} />
         </Switch>
         </main>
---
+++ aplikacja/src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+
+const Home = () => {
+
+return(
+    <>
+    <h1>Welcome to XyZ application!</h1>
+    <h2>Click on one user in sidenav to see information about him and his/hers albums</h2>
+    <h2>Do you want to see photos of one album? Just click on its title!</h2>
+    <h3>Have fun!</h3>
+    </>
+)
+}
+
+export default Home
